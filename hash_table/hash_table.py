7823467752a6e78df26351bfce0ebbed6426b040
Eli_ii <lichang@oregonstate.edu>
2022-11-10 22:20:48 +0800

    updated check, find, delete method.

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -16,17 +16,63 @@ class HashTable:
             ans = (ans * 263 + ord(c)) % 1000000007
         return ans % self.length
 
-    def add(self, string):
-        index = self._hash_func(string)
-        linked_list = self.arr[index]
+    def search(self, string, linked_list):
         node = linked_list.head
-        while (node != None and node.string != string):
+        # while (node != None and node.string != string):
+        while (node and node.string != string):
             node = node.next
+        return node
+
+    def lookup(self, string):
+        index = self._hash_func(string)
+        # linked_list = self.arr[index]
+        # return linked_list
+        return self.arr[index]
+
+    def add(self, string):
+        linked_list = self.lookup(string)
+        node = self.search(string, linked_list)
         if node == None:
             new_node = Node(string)
             linked_list.add_node(new_node)
     
+    def find(self, string):
+        linked_list = self.lookup(string)
+        return self.search(string, linked_list)
+        # if node == None:
+        #     return False
+        # else:
+        #     return True
+        
+    def delete(self, string):
+        linked_list = self.lookup(string)
+        node = linked_list.head
+        while (node and node.next and node.next.string != string):
+            node = node.next
+            
+        # if node == None or node.next == None:
+        #     return
+        # else:
+            # node.next = node.next.next
+        # if node != None and node.next != None:
+        if node and node.next:
+            node.set_next(node.next.next)
+            
+    def check(self, index):
+        # index = self._hash_func(string)
+        linked_list = self.arr[index]
+        # return linked_list
+        # return self.arr[index]
+        node = linked_list.head
+        string = ""
+        while node:
+            string += node.string
+            node = node.next
+        return string
+            
 
+
+            
 
 
 
